e932732e3a289ac46ee9710c8cb38b4fd7df66c7
NotAnotherDoorNob <38090078+NotAnotherDoorNob@users.noreply.github.com>
2024-01-24 15:21:22 -0500

    GSAP hero text animation

---
+++ home.js
@@ -0,0 +1,48 @@
+const line = '.line';
+let count = 0;
+const text = document.getElementById("text");
+const wordArray = [
+  "UX Researcher",
+  "UI Developer",
+  "D&D Enthusiast",
+  "Daft Punk Fan",
+  "Human Person",
+  "안녕하세요!",
+  "¡Olá!",
+  "Hello!"
+];
+
+gsap.set(line, {
+  y: 0, skewY: 0, stagger: {
+    amount: 0
+  }
+})
+
+const alternateText = () => {
+  gsap
+    .timeline({
+    repeat:-1 ,
+    delay: .5,
+    defaults: { ease: "expo.inOut", duration: 1}
+      })
+    .add('start')
+    .to(line, { y: 200, delay: 3 , skewY: 7,stagger: {
+    amount: 7
+  },  onComplete: onRepeat})
+    .to(line, { y: 0, delay: 1, skewY: 0, stagger: {
+    amount: 0
+  }})
+}
+
+window.onload = () => {
+	alternateText()
+};
+
+function onRepeat() {
+    if (count <= 6) {
+    count++;
+  } else {
+    count = 0;
+  }
+  text.innerHTML = wordArray[count];
+};
